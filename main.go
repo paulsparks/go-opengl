8016accc0b01eaf23e18541caa554f5a0604dc42
Paul Sparks <psparks1225@gmail.com>
2023-12-30 20:02:41 -0500

    render loop

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
+const WINDOW_WIDTH = 800
+const WINDOW_HEIGHT = 600
+
 func init() {
 	// make sure that main() runs on the main thread
 	runtime.LockOSThread()
@@ -20,20 +23,51 @@ func main() {
 	}
 	defer glfw.Terminate()
 
-	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	window, err := glfw.CreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Testing", nil, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	window.MakeContextCurrent()
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 
-	for !window.ShouldClose() {
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
+	window.MakeContextCurrent()
 
 	err = gl.Init()
 	if err != nil {
 		panic(err)
+	}
+
+	gl.Viewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+	window.SetFramebufferSizeCallback(framebuffer_size_callback)
+
+	gl.ClearColor(0.2, 0.3, 0.3, 1.0)
+
+	renderLoop(window)
+}
+
+func framebuffer_size_callback(window *glfw.Window, width int, height int) {
+	gl.Viewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+}
+
+func renderLoop(window *glfw.Window) {
+	for !window.ShouldClose() {
+		// input
+		processInput(window)
+
+		//rendering
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		// call events, swap buffers
+		glfw.PollEvents()
+		window.SwapBuffers()
+	}
+}
+
+func processInput(window *glfw.Window) {
+	if window.GetKey(glfw.KeyEscape) == glfw.Press {
+		window.SetShouldClose(true)
 	}
 }
